bdfaef9f5a307eff24984485c64e4296855a367c
iguglielmone <iguglielmone@pearljam.mecon.ar>
2018-03-27 12:57:50 -0300

    TP01-Commit05

---
+++ TPs/TP01/funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+float leerNumero(void);
+float funcionSuma(float num1,float num2);
+float funcionResta(float num1,float num2);
+float funcionMultiplicacion(float num1,float num2);
+float funcionDivision(float num1,float num2);
+float funcionFactorial(float num);
+int esEntero(float num);
+void imprimirResultado(float result);
+void imprimirError(int codigo);
+
+#endif // FUNCIONES_H_INCLUDED
