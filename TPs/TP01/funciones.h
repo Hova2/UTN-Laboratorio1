12a14b3f9ec24643ce42afb86c6503c2e7def2b1
Hova2 <nacho.mone@gmail.com>
2018-03-27 13:51:25 -0300

    TP01-Commit09

---
+++ TPs/TP01/funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+float leerNumero(void);
+float funcionSuma(float,float);
+float funcionResta(float,float);
+float funcionMultiplicacion(float,float);
+float funcionDivision(float,float);
+float funcionFactorial(float);
+char esEntero(float);
+void imprimirResultado(float);
+void imprimirError(int);
+
+#endif // FUNCIONES_H_INCLUDED
