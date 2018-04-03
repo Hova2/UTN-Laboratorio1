f1a44d419853e9d752ee6f72d06d5a92e9fb17f0
Juani <nacho.mone@gmail.com>
2018-04-03 11:45:46 -0300

    TP01-Commit16

--- TPs/TP01/funciones.h
+++ TPs/TP01/funciones.h
@@ -1,6 +1,23 @@
+/**
+ * @file funciones.h
+ * @author Juan Ignacio Guglielmone
+ * @date 14/04/2018
+ * @brief Archivo de funciones del TP01
+ *
+ * El archivo contiene la definiciond e todas las funciones del TP01.
+ *
+*/
+
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
+
+/** @brief Leer numero.
+ *
+ * Funcion que permite leer un numero desde la entrada estandar y devolverlo.
+ * @return Devuelve un numero del tipo float.
+ *
+ */
 float leerNumero(void);
 float funcionSuma(float,float);
 float funcionResta(float,float);
